5c2b97b944ca4fa03432467d6fe154780b88b9ce
青风百里 <qingfengbaili@icloud.com>
2019-10-25 09:17:01 +0800

    feat: Change layout

---
+++ resources/views/layouts/_header.blade.php
@@ -0,0 +1,5 @@
+<header class="container mx-auto h-12">
+    <nav class="flex justify-between flex-row items-center flex-wrap py-3">
+        <a href="" class="">Moreless</a>
+    </nav>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Moreless Blog') }}</title>
+    <title>@yield('title', 'Moreless Blog')</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
@@ -19,9 +19,12 @@
 
 <body>
     <div id="app" class="text-gray-900 antialiased leading-tight">
-        <main class="py-4">
+        @include('layouts._header')
+
+        <div class="container mx-auto px-4">
             @yield('content')
-        </main>
+        </div>
+
     </div>
 </body>
 
