8f6d9a63d9c4f4f9fd2134ab70943a978ae49660
青风百里 <qingfengbaili@icloud.com>
2019-11-03 15:12:34 +0800

    Fix css and js

--- resources/js/components/Search/hits/MusicHit.vue
+++ resources/js/components/Search/hits/MusicHit.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-row">
     <div
       @click="clickMusic"
-      class="mb-2 p-1 ml-2 bg-gray-200 text-xs rounded-lg cursor-pointer"
+      class="mb-2 p-1 mr-2 bg-gray-200 text-xs rounded-lg cursor-pointer"
     >{{ data.title }}-{{ data.artist }}</div>
   </div>
 </template>
