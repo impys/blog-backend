2edce9262ea46f558929ca3054c1e76169fcaf02
青风百里 <qingfengbaili@icloud.com>
2019-12-24 22:04:31 +0800

    audio spectrum visualization

--- resources/js/components/Welcome/Cards/CoverMedia/AudioMedia.vue
+++ resources/js/components/Welcome/Cards/CoverMedia/AudioMedia.vue
@@ -1,14 +1,71 @@
 <template>
-  <div class="bg-grey rounded-t">
-    <audio class="w-full" controls controlslist="nodownload" oncontextmenu="return false">
+  <div class="rounded-t relative">
+    <audio
+      :id="'audio-'+media.id"
+      class="w-full z-10"
+      controls
+      controlslist="nodownload"
+      @play="handlePlay"
+    >
       <source :src="media.url" />
     </audio>
+    <canvas
+      :id="'canvas-'+media.id"
+      class="absolute w-full h-full"
+      style="top:0;left:0px;z-index:-1"
+    ></canvas>
   </div>
-  <!-- TODO:频谱可视化 -->
 </template>
 <script>
+import Dancer from "vudio.js";
+
 export default {
-  props: ["media"]
+  props: ["media"],
+
+  data() {
+    return {
+      dancer: null
+    };
+  },
+
+  methods: {
+    handlePlay() {
+      if (!this.dancer) {
+        this.setDancer();
+      }
+      this.dancer.dance();
+      this.pauseAllOthers();
+    },
+    setDancer() {
+      let audio = document.querySelector(`#audio-${this.media.id}`);
+      let canvas = document.querySelector(`#canvas-${this.media.id}`);
+      let dancer = new Dancer(audio, canvas, {
+        effect: "waveform",
+        accuracy: 128,
+        waveform: {
+          maxHeight: 80,
+          minHeight: 1,
+          spacing: 1,
+          color: "#fb7299",
+          shadowBlur: 0,
+          shadowColor: "#fb7299",
+          fadeSide: true,
+          horizontalAlign: "center",
+          verticalAlign: "middle"
+        }
+      });
+      this.dancer = dancer;
+    },
+    pauseAllOthers() {
+      let audios = document.querySelectorAll("audio");
+      for (let index = 0; index < audios.length; index++) {
+        const audio = audios[index];
+        if (audio.id != `audio-${this.media.id}`) {
+          audio.pause();
+        }
+      }
+    }
+  }
 };
 </script>
 
