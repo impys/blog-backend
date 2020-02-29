7308b0aa38f69b6d6b65dfa8547d58a432730f02
青风百里 <qingfengbaili@icloud.com>
2020-02-29 23:55:12 +0800

    feat: optimized for post list

--- app/Http/Resources/PostList.php
+++ app/Http/Resources/PostList.php
@@ -14,6 +14,15 @@ class PostList extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        $data["id"] = $this->id;
+        $data["title"] = $this->title;
+        $data["is_top"] = $this->is_top;
+        $data["visited_count"] = $this->visited_count;
+        $data["created_at_human"] = $this->created_at_human;
+        $data["updated_at_human"] = $this->updated_at_human;
+        $data["cover_media"] = $this->cover_media;
+        $data["summary"] = $this->summary;
+
+        return $data;
     }
 }
