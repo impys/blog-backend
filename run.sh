8f76635208277de1e5f214d693670a2b14a5e0f9
青风百里 <qingfengbaili@icloud.com>
2020-04-22 21:28:16 +0800

    add migrate command

--- run.sh
+++ run.sh
@@ -3,6 +3,6 @@
 set -e
 
 echo "Caching configuration..."
-cd /srv && php artisan config:cache && php artisan route:cache && php artisan view:cache
+cd /srv && php artisan config:cache && php artisan route:cache && php artisan view:cache && php artisan migrate --force
 
 exec apache2-foreground
