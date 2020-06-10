186ab95926190676dfd0039d88a754c9565a9792
青风百里 <qingfengbaili@icloud.com>
2020-06-10 12:36:38 +0800

    feat: ignore img when get post's summary

--- app/Post.php
+++ app/Post.php
@@ -185,7 +185,7 @@ class Post extends Model
 
     public function getSummaryAttribute(): ?string
     {
-        preg_match("/<p>(.)*<\/p>/U", Markdown::parse($this->body), $results);
+        preg_match("/<p>((?!<img).)*<\/p>/U", Markdown::parse($this->body), $results);
 
         if (!count($results)) {
             return null;
