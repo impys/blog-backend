5a8005312e67360b1860850abf307f63ea53b584
青风百里 <qingfengbaili@icloud.com>
2020-06-13 01:26:11 +0800

    feat: support parse page container

--- nova-components/PostEditor/resources/js/md.js
+++ nova-components/PostEditor/resources/js/md.js
@@ -2,25 +2,25 @@ import MarkdownIt from 'markdown-it';
 var hljs = require('highlight.js');
 
 var md = new MarkdownIt({
-    html: false,
-    xhtmlOut: true,
-    breaks: true,
-    langPrefix: 'language-',
-    linkify: true,
-    typographer: false,
-    quotes: '“”‘’',
+  html: false,
+  xhtmlOut: true,
+  breaks: true,
+  langPrefix: 'language-',
+  linkify: true,
+  typographer: false,
+  quotes: '“”‘’',
 
-    highlight: function (str, lang) {
-        if (lang && hljs.getLanguage(lang)) {
-            try {
-                return '<pre class="hljs"><code>' +
-                    hljs.highlight(lang, str, true).value +
-                    '</code></pre>';
-            } catch (__) { }
-        }
-
-        return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  highlight: function (str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      try {
+        return '<pre class="hljs"><code>' +
+          hljs.highlight(lang, str, true).value +
+          '</code></pre>';
+      } catch (__) { }
     }
+
+    return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+  }
 });
 
 md.use(require('markdown-it-sub'));
@@ -29,44 +29,78 @@ md.use(require('markdown-it-sup'));
 
 md.use(require('markdown-it-footnote'));
 
+// 自定义 note 容器，用于创建提醒消息
 md.use(require('markdown-it-container'), 'note', {
 
-    validate: function (params) {
-        return true;
-    },
+  validate: function (param) {
+    return ['tip', 'warning', 'danger'].some(item => item === param.trim());
+  },
 
-    render: function (tokens, idx) {
-        if (tokens[idx].nesting === 1) {
-            return '<div class="note-base note-' + tokens[idx].info.trim() + '">' + '\n';
+  render: function (tokens, idx) {
+    if (tokens[idx].nesting === 1) {
+      return '<div class="note-base note-' + tokens[idx].info.trim() + '">' + '\n';
 
-        } else {
-            return '</div>\n';
-        }
+    } else {
+      return '</div>\n';
     }
+  }
 });
 
-// md.use(function (md) {
-//     md.core.ruler.push('anchor', state => {
-//         const tokens = state.tokens
-//         tokens.filter(token => {
-//             return token.type === 'heading_open' &&
-//                 (token.tag === 'h2' || token.tag === 'h3')
-//         }).forEach((token, index) => {
-//             const title = tokens[tokens.indexOf(token) + 1]
-//                 .children
-//                 .filter(token => token.type === 'text' || token.type === 'code_inline')
-//                 .reduce((accumulator, currentToken) => accumulator + currentToken.content.trim(), '')
+// 自定义 page 容器，用于内联跳转
+md.use(require('markdown-it-container'), 'page', {
 
-//             const id = 'toc' + (title + index).hashCode()
+  validate: function (param) {
+    return param.trim().match(/^page\s+(.*)$/);
+  },
 
-//             token.attrSet('id', id)
-//             token.attrSet('data-title', title)
-//             token.attrSet('data-id', id)
-//             token.attrSet('class', 'anchor')
-//         });
-//     })
-// });
+  render: function (tokens, idx) {
+    var m = tokens[idx].info.trim().match(/^page\s+(.*)$/);
+
+    if (tokens[idx].nesting === 1) {
+      let route = m[1].split(" ").reduce((carry, currentValue) => { return carry + '/' + currentValue }, '');
+
+      return `<div class="page" route=${route}>` + '\n';
+
+    } else {
+      return '</div>\n';
+    }
+  }
+});
+
+// 解析 h2 h3 标签，用于创建目录
+md.use(function (md) {
+  md.core.ruler.push('anchor', state => {
+    const tokens = state.tokens
+    tokens.filter(token => {
+      return token.type === 'heading_open' &&
+        (token.tag === 'h2' || token.tag === 'h3')
+    }).forEach((token, index) => {
+      const title = tokens[tokens.indexOf(token) + 1]
+        .children
+        .filter(token => token.type === 'text' || token.type === 'code_inline')
+        .reduce((accumulator, currentToken) => accumulator + currentToken.content.trim(), '')
+
+      const id = 'toc' + (title + index).hashCode()
+
+      token.attrSet('id', id)
+      token.attrSet('data-title', title)
+      token.attrSet('data-id', id)
+      token.attrSet('class', 'anchor')
+    });
+  })
+});
+
+// append _blank to a element
+md.use(require('markdown-it-for-inline'), 'url_new_win', 'link_open', function (tokens, idx) {
+  var aIndex = tokens[idx].attrIndex('target');
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']);
+  } else {
+    tokens[idx].attrs[aIndex][1] = '_blank';
+  }
+});
 
 export {
-    md
+  md
 }
+
